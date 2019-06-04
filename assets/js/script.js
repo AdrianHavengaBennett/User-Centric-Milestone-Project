52cd747f5f21687ecd1537e415c9664c0dc30eb7
Ubuntu <ubuntu@ip-172-31-92-129.ec2.internal>
2019-06-04 11:17:47 +0000

    added js navigation to entire project

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function menuToggle() {
+    let navElOne = document.getElementById("nav-bar");
+    let navElTwo = document.getElementById("nav-overlay");
+    navElOne.style.visibility = "visible";
+    navElTwo.style.visibility = "visible";
+    
+}
+
+function menuToggleMobile() {
+    let navElMobile = document.getElementById("nav-bar");
+    navElMobile.style.visibility = "visible";
+    
+}
+
+
