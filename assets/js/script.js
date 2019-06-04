6fe4b691312e8c775965ac18bd80e8c0cb5630c9
Ubuntu <ubuntu@ip-172-31-92-129.ec2.internal>
2019-06-04 16:26:43 +0000

    added animation to js nav toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
+function menuToggleMobile() {
+    let navElMobile = document.getElementById("nav-bar");
+    let navElMobileTwo = document.getElementById("nav-overlay");
+    navElMobile.style.visibility = "visible";
+    navElMobileTwo.style.visibility = "visible";
+    
+    let navPlayState = document.getElementById("nav-bar");
+    navPlayState.style.animationPlayState = "running";
+}
+
 function menuToggle() {
     let navElOne = document.getElementById("nav-bar");
     let navElTwo = document.getElementById("nav-overlay");
     navElOne.style.visibility = "visible";
     navElTwo.style.visibility = "visible";
     
+    let navPlayState = document.getElementById("nav-bar");
+    navPlayState.style.animationPlayState = "running";
 }
 
-function menuToggleMobile() {
-    let navElMobile = document.getElementById("nav-bar");
-    navElMobile.style.visibility = "visible";
-    
-}
+
 
 
